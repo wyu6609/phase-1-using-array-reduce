5a2e660c192e1ce61503d5d93d18b4c716869a21
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-02-02 12:59:39 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const callBack = (currentValue, previousValue) => currentValue + previousValue;
+const totalBatteries = batteryBatches.reduce(callBack);
